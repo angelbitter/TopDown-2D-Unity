232c087c574a22219e9a2e29f57c1832934ad881
Emilio Quintero Fernández <56022080+Emiquifer@users.noreply.github.com>
2025-06-27 22:21:12 +0200

    Condicion de victoria y end Menu

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,9 +4,15 @@ public class Item : MonoBehaviour, IInteractable
 {
     [SerializeField] private ItemSO itemData;
     [SerializeField] private GameManagerSO gameManager;
+    [SerializeField] private Canvas endCanvas;
     public void Interact()
     {
         gameManager.Inventory.AddItem(itemData);
         Destroy(gameObject);
+
+        if(itemData.name.Equals("Trophy"))
+        {
+            GameManager.Instance.EndGame(endCanvas);
+        }
     }
 }
